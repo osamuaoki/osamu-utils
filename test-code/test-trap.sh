7fc9215a0d31e08d5f33d2b675017b608baea52c
Osamu Aoki <osamu@debian.org>
2017-11-05 13:56:17 +0900

    More trap comments
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- test-code/test-trap.sh
+++ test-code/test-trap.sh
@@ -2,38 +2,48 @@
 #
 # Copyright 2016 Osamu Aoki
 #
+# see "kill -l", signal(7)
+# +  0 EXIT: on exit
+# *  1 HUP:  hang-up (^\)
+# *  2 INT:  KB interrupt (^C)
+#    3 QUIT: QUIT (^D)
+#    7 BUS:  buffer overflow etc.
+#   10 USR1: user defined
+# * 13 PIPE: broken pipe
+# * 15 TERM: trappable termination
+# No trap for KILL(9)
+showsig0() {
+EXITCODE=$?
+SIG=0
+echo "SIGEXIT SIG=$SIG, EXIT=$EXITCODE"
+exit $EXITCODE
+}
+
 showsig1() {
 EXITCODE=$?
 SIG=1
-echo "SIG=$SIG, EXIT=$EXITCODE"
+echo "SIGHUP  SIG=$SIG, EXIT=$EXITCODE"
 exit $EXITCODE
 }
 
 showsig2() {
 EXITCODE=$?
 SIG=3
-echo "SIG=$SIG, EXIT=$EXITCODE"
+echo "SIGINT  SIG=$SIG, EXIT=$EXITCODE"
 exit $EXITCODE
 }
 
 showsig3() {
 EXITCODE=$?
 SIG=3
-echo "SIG=$SIG, EXIT=$EXITCODE"
+echo "SIGQUIT SIG=$SIG, EXIT=$EXITCODE"
 exit $EXITCODE
 }
 
-showsig0() {
+showsig13() {
 EXITCODE=$?
-SIG=0
-echo "SIG=$SIG, EXIT=$EXITCODE"
-exit $EXITCODE
-}
-
-showsig4() {
-EXITCODE=$?
-SIG=4
-echo "SIG=$SIG, EXIT=$EXITCODE"
+SIG=13
+echo "SIGPIPE SIG=$SIG, EXIT=$EXITCODE"
 exit $EXITCODE
 }
 
@@ -41,10 +51,13 @@ trap showsig0 0
 trap showsig1 1
 trap showsig2 2
 trap showsig3 3
+trap showsig7 7
 
 if [ -n "$1" ]; then
 echo "FALSE"
 false
+# -e causes exit
+echo "NEVER HERE"
 fi
 echo "============"
 
