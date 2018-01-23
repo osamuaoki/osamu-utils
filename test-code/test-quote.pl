6f366496380e4ad129699a48c1d31007aac53426
Osamu Aoki <osamu@debian.org>
2018-01-24 00:06:28 +0900

    More test
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- test-code/test-quote.pl
+++ test-code/test-quote.pl
@@ -6,7 +6,8 @@ my $foo1 = "ba1r";
 my $foo2 = "ba2r";
 my $foo3 = "ba3r";
 
-print "$foo1-$foo2$foo3\n";
-print "$foo1\\$foo2$foo3\n";
-print "$foo1/$foo2$foo3\n";
-print "$foo1\t$foo2$foo3\n";
+print "$foo1-$foo2.$foo3\n";
+print "$foo1\\$foo2'$foo3\n";
+print "$foo1\\$foo2" . "_$foo3\n";
+print "$foo1/$foo2,$foo3\n";
+print "$foo1\t$foo2:$foo3\n";
